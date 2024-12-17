9acdd27495b329e20707f4e9986a14c797d27067
Jorge <jorgomez@protonmail.com>
2024-12-17 21:43:57 +0000

    “update”

--- src/Components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/Components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,18 +1,23 @@
 // import { useColorMode, Button, IconButton } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
-import { IconButton, useColorMode } from '@chakra-ui/react'
+import { IconButton, Tooltip, useColorMode } from '@chakra-ui/react'
 
 export const ThemeSwitcher = () => {
-  const { colorMode, toggleColorMode } = useColorMode() // Chakra hook para gestionar temas
+  const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <IconButton
-      aria-label='Toggle theme'
-      icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-      onClick={toggleColorMode}
-      variant='ghost'
-      size='md'
-      isRound
-    />
+    <Tooltip
+      label={`Switch to ${colorMode === 'light' ? 'dark' : 'light'} mode`}
+      hasArrow
+    >
+      <IconButton
+        aria-label='Toggle theme'
+        icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+        onClick={toggleColorMode}
+        variant='ghost'
+        size='md'
+        isRound
+      />
+    </Tooltip>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './Components/Theme/Theme.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     </ChakraProvider>
   </BrowserRouter>
 )
